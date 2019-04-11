162971a7aa488b3893cdda115a5cd70ad12aa5ee
LieneKirsfelde <47639294+LieneKirsfelde@users.noreply.github.com>
2019-04-11 20:25:01 +0300

    Add files via upload

---
+++ MathOperations.java
@@ -0,0 +1,19 @@
+package lv.javaguru.lesson2;
+
+public class MathOperations {
+    public static void main(String[] args) {
+
+        int firstNumber = 10;
+        int seconNumber = 5;
+
+        int sumResult = firstNumber + seconNumber;
+        int multipyResult = firstNumber * seconNumber;
+        int subtractResult = firstNumber - seconNumber;
+        int divideRstult = firstNumber / seconNumber;
+
+        System.out.println("Sum result: " + sumResult);
+        System.out.println("Subtracting result: " + subtractResult);
+        System.out.println("multiply result: " + multipyResult);
+        System.out.println("divide result: " + divideRstult);
+    }
+}
